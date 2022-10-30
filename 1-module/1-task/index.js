badb685c2f55e114843793c62570627018f09158
Grigorii Kononov <gkononov16@gmail.com>
2022-10-30 19:19:16 +0300

    Выполнены следующие задания: -модуль 0, задание1; -модуль 1, задание 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  //без использования рекурсии
+  /*let result = 1;
+  for (let i = n; i > 0; i--) {
+    result *= i;
+  }
+  return result;*/
+  return n ? n * factorial(n - 1) : 1;
 }
+
