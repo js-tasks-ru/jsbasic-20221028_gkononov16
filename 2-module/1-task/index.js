f0657a1f9608ca03e6921aa4cb7e83cb79a2bcf1
Grigorii Kononov <gkononov16@gmail.com>
2022-11-15 18:25:58 +0300

    Выполнены задания модуля 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,8 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (Number.isInteger(salaries[key]) && !isNaN(salaries[key])) 
+      sum += salaries[key];    
+  }
+  return sum;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,16 @@
 let calculator = {
-  // ваш код
+  read(a, b) {
+    this.a = a;
+    a = +prompt("Число 1", 0);
+    this.b = b;
+    b = +prompt("Число 2", 0);
+  },
+  sum() {
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
 };
-
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
